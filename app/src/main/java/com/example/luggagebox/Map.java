5fee13a113f248c8ba653756a82f7b7f2ef2142e
KimJuneGeun <junegeun.kim@gmail.com>
2020-12-04 21:03:02 +0900

    Drawer NavigationView 추가

--- app/src/main/java/com/example/luggagebox/Map.java
+++ app/src/main/java/com/example/luggagebox/Map.java
@@ -1,27 +1,58 @@
 package com.example.luggagebox;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
-import com.naver.maps.geometry.LatLng;
-import com.naver.maps.map.CameraAnimation;
-import com.naver.maps.map.CameraUpdate;
-import com.naver.maps.map.MapFragment;
-import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.OnMapReadyCallback;
-import com.naver.maps.map.overlay.Marker;
+public class Map extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class Map extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private TextView textView;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        textView = findViewById(R.id.textView);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    public void btn_menu(View view) {
+        if (view.getId() == R.id.btn_menu) {  // Buttoon의 ID를 찾아서 실행이 된다.
+            drawerLayout.openDrawer(navigationView);
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav1:
+                break;
+            case R.id.nav2:
+                break;
+            case R.id.nav3:
+                break;
+            case R.id.nav4:
+                break;
+            case R.id.nav5:
+                Intent it = new Intent(Map.this, manager_webview.class);
+                startActivity(it);
+                break;
+        }
 
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
